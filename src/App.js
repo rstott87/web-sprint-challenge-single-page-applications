7908145016a371565c8174d075b45896c99ea002
rstott87 <rstott87@yahoo.com>
2021-12-17 11:29:12 -0500

    completed pizza form in Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom';
+import Form from './Form'
 
 const App = () => {
   return (
     <>
+      <nav> 
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+        <Link to="/">
+          <div className="home">Home</div>
+        </Link>
+        <Link to="/pizza">
+          <div className="order-pizza">Order Pizza</div>
+        </Link>
+        <Route path ="/pizza">
+          <Form></Form>
+        </Route>
+      </nav>
+
+      <Route exact path="/">
+      </Route>
     </>
   );
 };
---
+++ src/Form.js
@@ -0,0 +1,48 @@
+import {axios} from 'axios';
+import * as yup from 'yup';
+
+
+
+export default function Form (props) {
+    return (
+    <>
+        <h3>Pizza Form</h3>
+        <form id = "pizza-form">
+            <label>
+                Name:
+                <input type="text" id="name-input">
+                </input>
+            </label> <br/>
+            <label>
+                Size:
+                <select id="size-dropdown">
+                    <option value= "small">Small</option>
+                    <option value= "medium">Medium</option>
+                    <option value= "large">Large</option>
+                </select>   
+            </label><br/>
+            <label> Pepperoni:
+                <input name= "pepperoni" type="checkbox"></input>
+            </label><br/>
+            <label> 
+                Bacon:
+                <input name= "bacon" type="checkbox"></input>
+            </label>
+            <label> 
+                Pineapple:
+                <input name= "pineapple" type="checkbox"></input>
+            </label>
+            <label> 
+            Vegetable:
+                <input name= "vegetable" type="checkbox"></input>
+            </label><br/>
+            <label>
+                Special Instructions:
+                <input type="text" id="special-text">
+                </input>
+            </label> 
+            <button id="order-button">Place Order</button>
+        </form>
+    </>
+    )
+}
